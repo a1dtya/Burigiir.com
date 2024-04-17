aac589bc28e8ff5a9b14f4077649f4c753f138ec
Aaditya Parmar <94757097+a1dtya@users.noreply.github.com>
2024-04-17 14:43:34 +0530

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,26 +5,26 @@ const Footer = () => {
   return (
     <footer>
       <div>
-        <h2>MBA Burger Wala</h2>
+        <h2>Burgiirr - You can hear the picture isn't it?</h2>
 
         <p>We are trying to give you the best taste possible.</p>
         <br />
 
         <em>We give attention to genuine feedback.</em>
 
-        <strong>All right received @mbaburgerwala</strong>
+        <strong>All right received @Burgiir</strong>
       </div>
 
       <aside>
         <h4>Follow Us</h4>
 
-        <a href="https://youtube.com/6packprogrammer">
+        <a href="https://www.linkedin.com/in/aadityaparmar/">
           <AiFillYoutube />
         </a>
-        <a href="https://instagram.com/meabhisingh">
+        <a href="https://twitter.com/a1dtya">
           <AiFillInstagram />
         </a>
-        <a href="https://github.com/meabhisingh">
+        <a href="https://github.com/a1dtya">
           <AiFillGithub />
         </a>
       </aside>
